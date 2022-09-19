d430de9a184d1c63b62d614c73f49a5b0da84bb3
Choi Min Kyu <ohiseaster@gmail.com>
2022-09-19 13:47:32 +0900

    [코드 추가] 일단 모두 요청 하는 걸로 추가
     * 코드 정리 필요
         * 반복 코드 남아 있음.

--- TMDB_LIST/TMDB_LIST/View/Controller/VideoViewController.swift
+++ TMDB_LIST/TMDB_LIST/View/Controller/VideoViewController.swift
@@ -23,7 +23,7 @@ extension VideoViewController {
     private enum Constants {
         static let cellHeight: CGFloat = 200
         static let headerCellHeight: CGFloat = 40
-        static let headerViewHeight: CGFloat = 500
+        static let headerViewHeight: CGFloat = 250
     }
 }
 
@@ -84,8 +84,30 @@ final class VideoViewController: UIViewController {
 
 extension VideoViewController: ViewModelBindableType {
     func bindInput(viewModel: MovieViewModel) {
-        self.rx.viewDidLoad
+        let viewDidLoad = self.rx.viewDidLoad.share()
+        
+        viewDidLoad
             .map{ _ in .load }
+            .bind(to: viewModel.input)
+            .disposed(by: self.disposeBag)
+        
+        viewDidLoad
+            .map { _ in .load2 }
+            .bind(to: viewModel.input)
+            .disposed(by: self.disposeBag)
+        
+        viewDidLoad
+            .map { _ in .load3 }
+            .bind(to: viewModel.input)
+            .disposed(by: self.disposeBag)
+        
+        viewDidLoad
+            .map { _ in .load4 }
+            .bind(to: viewModel.input)
+            .disposed(by: self.disposeBag)
+        
+        viewDidLoad
+            .map { _ in .load5 }
             .bind(to: viewModel.input)
             .disposed(by: self.disposeBag)
     }
@@ -147,117 +169,117 @@ extension VideoViewController {
 
 //MARK: - Delegates for UITableViewDelegate, UITableViewDataSource
 
-extension VideoViewController: UITableViewDelegate, UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return sectionTitles.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCEll.identifier, for: indexPath) as! CollectionViewTableViewCEll
-        
-        cell.delegate = self
-        
-        switch indexPath.section {
-        case Section.Movie.rawValue:
-            SimpleAPI.shared.movies {
-                switch $0 {
-                case .success(let movies):
-                    let movieViewModels = movies.map {
-                        MovieViewModel(movieModel: $0)
-                    }
-                    
-                    cell.configure(with: movieViewModels)
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
-        case Section.TV.rawValue:
-            SimpleAPI.shared.tv {
-                switch $0 {
-                case .success(let movies):
-                    let tvViewModels = movies.map {
-                        MovieViewModel(movieModel: $0)
-                    }
-                    cell.configure(with: tvViewModels)
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
-        case Section.Popular.rawValue:
-            SimpleAPI.shared.popular {
-                switch $0 {
-                case .success(let movies):
-                    let tvViewModels = movies.map {
-                        MovieViewModel(movieModel: $0)
-                    }
-                    cell.configure(with: tvViewModels)
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
-        case Section.CommingSoon.rawValue:
-            SimpleAPI.shared.commingSoon {
-                switch $0 {
-                case .success(let movies):
-                    let tvViewModels = movies.map {
-                        MovieViewModel(movieModel: $0)
-                    }
-                    cell.configure(with: tvViewModels)
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
-        case Section.TopRates.rawValue:
-            SimpleAPI.shared.topRates {
-                switch $0 {
-                case .success(let movies):
-                    let tvViewModels = movies.map {
-                        MovieViewModel(movieModel: $0)
-                    }
-                    cell.configure(with: tvViewModels)
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
-        default:
-            fatalError()
-        }
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        Constants.cellHeight
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        Constants.headerCellHeight
-    }
-    
-    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        guard let header = view as? UITableViewHeaderFooterView else { return }
-        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
-        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 200, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
-        header.textLabel?.text = header.textLabel?.text?.lowercased().capitalized
-        header.textLabel?.textColor = .red
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return self.sectionTitles[section]
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let defaultOffset = self.view.safeAreaInsets.top
-        let offset = scrollView.contentOffset.y + defaultOffset
-
-        self.navigationController?.navigationBar.transform = .init(translationX: 0, y: -offset)
-        
-    }
-}
+//extension VideoViewController: UITableViewDelegate, UITableViewDataSource {
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return sectionTitles.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCEll.identifier, for: indexPath) as! CollectionViewTableViewCEll
+//
+//        cell.delegate = self
+//
+//        switch indexPath.section {
+//        case Section.Movie.rawValue:
+//            SimpleAPI.shared.movies {
+//                switch $0 {
+//                case .success(let movies):
+//                    let movieViewModels = movies.map {
+//                        MovieViewModel(movieModel: $0)
+//                    }
+//
+//                    cell.configure(with: movieViewModels)
+//                case .failure(let error):
+//                    print(error.localizedDescription)
+//                }
+//            }
+//        case Section.TV.rawValue:
+//            SimpleAPI.shared.tv {
+//                switch $0 {
+//                case .success(let movies):
+//                    let tvViewModels = movies.map {
+//                        MovieViewModel(movieModel: $0)
+//                    }
+//                    cell.configure(with: tvViewModels)
+//                case .failure(let error):
+//                    print(error.localizedDescription)
+//                }
+//            }
+//        case Section.Popular.rawValue:
+//            SimpleAPI.shared.popular {
+//                switch $0 {
+//                case .success(let movies):
+//                    let tvViewModels = movies.map {
+//                        MovieViewModel(movieModel: $0)
+//                    }
+//                    cell.configure(with: tvViewModels)
+//                case .failure(let error):
+//                    print(error.localizedDescription)
+//                }
+//            }
+//        case Section.CommingSoon.rawValue:
+//            SimpleAPI.shared.commingSoon {
+//                switch $0 {
+//                case .success(let movies):
+//                    let tvViewModels = movies.map {
+//                        MovieViewModel(movieModel: $0)
+//                    }
+//                    cell.configure(with: tvViewModels)
+//                case .failure(let error):
+//                    print(error.localizedDescription)
+//                }
+//            }
+//        case Section.TopRates.rawValue:
+//            SimpleAPI.shared.topRates {
+//                switch $0 {
+//                case .success(let movies):
+//                    let tvViewModels = movies.map {
+//                        MovieViewModel(movieModel: $0)
+//                    }
+//                    cell.configure(with: tvViewModels)
+//                case .failure(let error):
+//                    print(error.localizedDescription)
+//                }
+//            }
+//        default:
+//            fatalError()
+//        }
+//
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        Constants.cellHeight
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        Constants.headerCellHeight
+//    }
+//
+//    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+//        guard let header = view as? UITableViewHeaderFooterView else { return }
+//        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+//        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 200, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+//        header.textLabel?.text = header.textLabel?.text?.lowercased().capitalized
+//        header.textLabel?.textColor = .red
+//    }
+//
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return self.sectionTitles[section]
+//    }
+//
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let defaultOffset = self.view.safeAreaInsets.top
+//        let offset = scrollView.contentOffset.y + defaultOffset
+//
+//        self.navigationController?.navigationBar.transform = .init(translationX: 0, y: -offset)
+//
+//    }
+//}
 
 // MARK: - Custom Delegates
 
